e786a80536ad768c9a4e9c335798aa90deb539ce
abhin.rustagi <rustagi.abhin@gmail.com>
2022-08-12 02:38:20 +0530

    chore: setup with husky hooks, styled components & eslint

---
+++ src/theme/styled.d.ts
@@ -0,0 +1,14 @@
+import { CSSProp } from 'styled-components'
+
+import ITheme from './theme'
+
+declare module 'styled-components' {
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface DefaultTheme extends ITheme {}
+}
+
+declare module 'react' {
+	interface Attributes {
+		css?: CSSProp<ITheme>
+	}
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,5 @@
+export interface ITheme {
+	colors: {
+		black: '#000'
+	}
+}
